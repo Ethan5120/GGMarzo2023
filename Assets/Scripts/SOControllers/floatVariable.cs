8293ea477b81760b2bc9e685086ccb163502e540
Ethan5120 <123921592+Ethan5120@users.noreply.github.com>
2023-03-17 22:14:47 -0600

    gonorrea

---
+++ Assets/Scripts/SOControllers/floatVariable.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/Float Variable", order = 1)]
+
+public class floatVariable : ScriptableObject
+{
+    public float floatValue;
+}
---
+++ Assets/Scripts/bulletEnemyFather.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletEnemyFather : PooledObject
+{
+    private Vector2 moveDirection;
+    [SerializeField] float moveSpeed;
+    private float damage = 1;
+
+
+    void Update()
+    {
+        transform.Translate(moveDirection * moveSpeed * Time.deltaTime);
+    }
+
+    public void SetMoveDirection(Vector2 dir)
+    {
+        moveDirection = dir;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.TryGetComponent<Player>(out Player playerComponent))
+        {
+            playerComponent.TakeDamage(damage);
+            Release();
+        }
+    }
+}
