3da2097babc0855219e97cfcbb4f4ec7759196b1
QuiqueYunge <114827533+QuiqueYunge@users.noreply.github.com>
2023-03-18 03:53:40 -0600

    moveplayer

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
-{ 
+{
+    Vector3 mousePos;
+    public float minY, maxY;
+    public float minX, maxX;
     public enum PolarityState{CYAN, ORANGE, PURPLE};
 
     [SerializeField]
@@ -19,7 +22,11 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Debug.Log(mousePos);
+        float posYlimited = Mathf.Clamp(mousePos.y, minY, maxY);
+        float posXlimited = Mathf.Clamp(mousePos.x, minX, maxX);
+        transform.position = new Vector3(posXlimited, posYlimited, 0);
     }
     public void TakeDamage(float damageAmmount)
     {
