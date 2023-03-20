33e1a2103af32f2bd6e1a64c8bc3b151b83f5e7c
Ethan5120 <123921592+Ethan5120@users.noreply.github.com>
2023-03-19 19:14:03 -0600

    Update EnemyFactory.cs

--- Assets/Scripts/Factorys/EnemyFactory.cs
+++ Assets/Scripts/Factorys/EnemyFactory.cs
@@ -9,7 +9,7 @@ public class EnemyFactory : Factory
 
     public override IProduct GetProduct(Vector3 position)
     {
-        ePool = GameObject.FindObjectOfType<EnemyPool>();
+        //ePool = GameObject.FindObjectOfType<EnemyPool>();
         EnemyParent enemyPrefab = (EnemyParent)ePool.Get();
 
         enemyPrefab.transform.position = position;
