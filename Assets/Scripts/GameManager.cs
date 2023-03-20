70928af249fe561ff043ee4d7d7f05d83c1c056f
Ethan5120 <123921592+Ethan5120@users.noreply.github.com>
2023-03-20 10:20:34 -0600

    LLLLL

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,10 +7,13 @@ public class GameManager : MonoBehaviour
 
     [SerializeField] boolVariable isWavesCompleted;
     [SerializeField] BossFactory bossFactory;
+    [SerializeField] intVariable bCharges;
+    [SerializeField] SpriteRenderer[] bChargesDisplay;
+    [SerializeField] Sprite[] bChargesSprites;
     bool HasBossSpawned = false;
     void Start()
     {
-        
+      
     }
 
     void Update()
@@ -20,6 +23,27 @@ public class GameManager : MonoBehaviour
             HasBossSpawned = true;
             BossController thisenemy = (BossController) bossFactory.GetProduct(new Vector2(0f, 6f));
 
+        }
+
+        switch (bCharges.intValue)
+        {
+            case 0:
+                {
+                    bChargesDisplay[bCharges.intValue].sprite = bChargesSprites[0];
+                    break;
+                }
+
+            case 1:
+                {
+                    bChargesDisplay[bCharges.intValue].sprite = bChargesSprites[0];
+                    break;
+                }
+
+            case 2:
+                {
+                    bChargesDisplay[bCharges.intValue].sprite = bChargesSprites[0];
+                    break;
+                }
         }
     }
 }
