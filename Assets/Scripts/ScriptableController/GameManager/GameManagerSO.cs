197ea16aa2c7ee073ca0d0ab1cec2d0621fe9a49
Ethan5120 <123921592+Ethan5120@users.noreply.github.com>
2024-04-30 13:06:07 -0600

    Updates

---
+++ Assets/Scripts/ScriptableController/GameManager/GameManagerSO.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerSO : ScriptableObject
+{
+    [Header("GameState")]
+    public float gameTime = 1;
+    public enum GameState {MainMenu, GameWavesRunning, GameBoss, GamePaused, GameWin, GameLose, SecretFound}
+    [Space(5)]
+
+    [Header("PlayerData")]
+    
+
+
+}
