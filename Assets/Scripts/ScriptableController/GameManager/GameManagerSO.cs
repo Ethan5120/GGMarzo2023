d436efa948e9e81ddcb72417b38b7fdecbdc095a
Ethan5120 <123921592+Ethan5120@users.noreply.github.com>
2024-05-09 14:10:30 -0600

    Player In Progres

--- Assets/Scripts/ScriptableController/GameManager/GameManagerSO.cs
+++ Assets/Scripts/ScriptableController/GameManager/GameManagerSO.cs
@@ -6,11 +6,25 @@ public class GameManagerSO : ScriptableObject
 {
     [Header("GameState")]
     public float gameTime = 1;
-    public enum GameState {MainMenu, GameWavesRunning, GameBoss, GamePaused, GameWin, GameLose, SecretFound}
+    public enum GameState {MainMenu, GameWavesRunning, GamePaused, ResultsScreen}
+    public GameState cGState;
+    public enum StageState{EnemiesStage, BossStage, TutorialStage}
+    public StageState cStageState;
     [Space(5)]
 
     [Header("PlayerData")]
     public float playerHealth = 5;
+    public float playerMaxHP = 10;
+    public int bombCharges = 1;
+    [Space(5)]
 
+
+    [Header("AudioData")]
+    public AudioSource[] gameMusic;
+    [Space(5)]
+
+    [Header("ScoreData")]
+    public float currentScore;
+    public float currentHiScore;    
 
 }
