5fe98d0569fe756a476e4c9e0d58528beb78588b
QuiqueYunge <114827533+QuiqueYunge@users.noreply.github.com>
2024-05-09 09:24:07 -0600

    menu .7
    roberto es puto

--- Assets/MenuManager.cs
+++ Assets/MenuManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
@@ -16,5 +17,31 @@ public class MenuManager : MonoBehaviour
         Application.Quit();
     }
 
+    private bool pantallaCompleta = true;
+
+    void Start()
+    {
+        // Agregar un listener al botón para detectar clics
+        Button btn = GetComponent<Button>();
+        btn.onClick.AddListener(ToggleModoPantalla);
+    }
+
+    void ToggleModoPantalla()
+    {
+        pantallaCompleta = !pantallaCompleta;
+
+        // Cambiar entre modo pantalla completa y modo ventana
+        if (pantallaCompleta)
+        {
+            Screen.SetResolution(Screen.currentResolution.width, Screen.currentResolution.height, true);
+        }
+        else
+        {
+            // Aquí puedes definir la resolución deseada para el modo ventana
+            int ventanaAncho = 800;
+            int ventanaAlto = 600;
+            Screen.SetResolution(ventanaAncho, ventanaAlto, false);
+        }
+    }
 
 }
